8804156397b8335fa6ff34ef4e080db5f786f763
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2024-12-12 23:58:26 +0300

    Add tests base

---
+++ src/WebSocketChat.Tests/Messages/MessagesTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace WebSocketChat.Tests.Messages
+{
+    public class MessagesTests
+    {
+        [Fact]
+        public void HandleMessage_WhenMessageReceived_MessageIsSavedIntoDb()
+        {
+            Thread.Sleep(432);
+            Assert.Equal(2*2, 4);
+        }
+
+        [Fact]
+        public void GetMessages_WhenCalled_ReturnsMessages()
+        {
+            Thread.Sleep(143);
+            Assert.Equal(2 * 2, 4);
+        }
+    }
+}
